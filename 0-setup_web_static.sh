33a7541aeadc1174edbcd8cba71f3dffcf284f80
Oluwatobi <amure387@gmail.com>
2023-04-05 15:46:00 +0000

    feat: completed web_static  deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-if [ command nginx] | grep 'not found'; then
+apt-get update
+if ! command -v nginx &> /dev/null; then
   sudo apt-get install -y nginx;
 fi
 if [ ! -d '/data/' ]; then
@@ -18,7 +19,15 @@ fi
 if [ ! -d '/data/web_static/releases/test/' ]; then
   sudo mkdir -p /data/web_static/releases/test/;
 fi
-echo 'Are you a sphinx or a white lion, make up your mind Kimba' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu /data/
-sed -i.bak '/listen 80 default_server/a \tlocation \/hbnb_static {\n\talias /data/web_static/current/;\n\t}' menginx
+sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R $USER:$USER /data/
+sed -i.bak '/listen 80 default_server/a \\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
