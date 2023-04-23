707c6e69a25a9262f77145134749a7f07a6db497
Oluwatobi <amure387@gmail.com>
2023-04-23 10:13:35 +0000

    fix: explicitly stated file storage

--- models/city.py
+++ models/city.py
@@ -10,9 +10,10 @@ from .base_model import Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship(
+    if HBNB_TYPE_STORAGE == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship(
         "Place", cascade="all, delete-orphan", backref="cities")
     else:
         state_id = ""
