268c5bd065e30381feffa87bb24b45e53ff69b10
Oluwatobi <amure387@gmail.com>
2023-05-17 09:05:07 +0000

    feat: changed route(task 0)

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 '''Module for a script that starts a Flask web application'''
 from flask import Flask
-myapp = Flask(__name__)
+app = Flask(__name__)
 
 
-@myapp.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def greet():
     '''Run on route'''
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    myapp.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
